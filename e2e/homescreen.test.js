45d81e19a50a9f27282d3538ce45df57256d5a24
Vanessa Soares <vsoares@insider.com>
2024-06-25 11:22:19 -0300

    second commit

---
+++ e2e/homescreen.test.js
@@ -0,0 +1,18 @@
+
+const { openApp } = require('./utils/openApp');
+
+
+describe('Home screen', () => {
+  beforeEach(async () => {
+     await openApp(); 
+  });
+
+  it('"Click me" button should be visible', async () => {
+    await expect(element(by.id('click-me-button'))).toBeVisible();
+  });
+
+  it('shows "Hi!" after tapping "Click me"', async () => {
+    await element(by.id('click-me-button')).tap();
+    await expect(element(by.text('Hi!'))).toBeVisible();
+  });
+});
